421e65377ca5b8d047ff334fb241e2cbef512502
Bruno Fernandes <brunofdsleite@gmail.com>
2025-05-14 16:04:30 -0300

    add carousel component

---
+++ app/src/main/java/me/fernandesleite/mahoulist/feature/anime/presentation/component/BigImageCarousel.kt
@@ -0,0 +1,142 @@
+package me.fernandesleite.mahoulist.feature.anime.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import me.fernandesleite.mahoulist.feature.anime.data.model.remote.common.Anime
+
+@Composable
+fun BigImageCarousel(
+    title: String,
+    animeList: List<Anime> = listOf(),
+    onClickItem: (Anime) -> Unit = {},
+) {
+    val state = rememberPagerState(0) {
+        animeList.count()
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            modifier = Modifier
+                .align(Alignment.Start),
+            text = title,
+            fontWeight = FontWeight.Medium,
+            style = MaterialTheme.typography.titleLarge,
+        )
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            HorizontalPager(
+                state = state
+                ) { i ->
+                val carouselItem = animeList[i]
+                Surface(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .height(150.dp)
+                        .shadow(8.dp, RoundedCornerShape(16.dp))
+                        .clickable {
+                            onClickItem(animeList[state.currentPage])
+                        }
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        AsyncImage(
+                            model = carouselItem.mainPicture.large,
+                            contentScale = ContentScale.Crop,
+                            contentDescription = carouselItem.title
+                        )
+                        Text(
+                            text = carouselItem.title,
+                            maxLines = 2,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 8.dp)
+                        )
+                    }
+                }
+            }
+
+        }
+        DotsIndicator(
+            modifier = Modifier
+                .padding(top = 8.dp),
+            state.pageCount,
+            state.currentPage
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun IndicatorDot(
+    color: Color = MaterialTheme.colorScheme.primary
+) {
+    Box(
+        modifier = Modifier
+            .size(10.dp)
+            .clip(CircleShape)
+            .background(color),
+        content = {}
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DotsIndicator(
+    modifier: Modifier = Modifier,
+    totalDots: Int = 0,
+    selectedDot: Int = 0
+) {
+    val selectedColor: Color = MaterialTheme.colorScheme.primary
+    val unSelectedColor: Color = MaterialTheme.colorScheme.secondary
+    LazyRow(
+        modifier = modifier
+            .wrapContentSize()
+            .background(MaterialTheme.colorScheme.surface),
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+
+    ) {
+        items(totalDots) { currentDot ->
+            if (currentDot == selectedDot) {
+                IndicatorDot(color = selectedColor)
+            } else
+                IndicatorDot(color = unSelectedColor)
+        }
+    }
+}
--- app/src/main/java/me/fernandesleite/mahoulist/feature/anime/presentation/screen/HomeScreen.kt
+++ app/src/main/java/me/fernandesleite/mahoulist/feature/anime/presentation/screen/HomeScreen.kt
@@ -2,13 +2,18 @@ package me.fernandesleite.mahoulist.feature.anime.presentation.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import me.fernandesleite.mahoulist.core.ui.UiState
+import me.fernandesleite.mahoulist.feature.anime.presentation.component.BigImageCarousel
 import me.fernandesleite.mahoulist.feature.anime.presentation.component.HorizontalScroller
 import me.fernandesleite.mahoulist.feature.anime.presentation.viewmodel.AnimeViewModel
 
@@ -27,25 +32,36 @@ fun HomeScreen(
     val popularList by viewModel.popularList.collectAsState()
     val currentlyAiringList by viewModel.currentlyAiringList.collectAsState()
 
-
     Column(
-        verticalArrangement = Arrangement.spacedBy(32.dp)
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .padding(top = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
+        BigImageCarousel(title = "Discover", animeList = currentlyAiringList) {
+            // TODO: Navigate to anime details
+        }
         HorizontalScroller(
             title = "Currently Airing",
             isLoading = currentlyAiringList.isEmpty(),
             animeList = currentlyAiringList
-        )
+        ) {
+            // TODO: Navigate to anime details
+        }
         HorizontalScroller(
             title = "Popular",
             isLoading = currentlyAiringList.isEmpty(),
             animeList = popularList
-        )
+        ) {
+            // TODO: Navigate to anime details
+        }
         HorizontalScroller(
             title = "Suggested",
             isLoading = currentlyAiringList.isEmpty(),
             animeList = suggestedList
-        )
+        ) {
+            // TODO: Navigate to anime details
+        }
     }
 
 }
